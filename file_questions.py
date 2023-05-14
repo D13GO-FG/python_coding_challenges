a85720a3069ef849b64548b248ba399706f87ecb
Diego_Flores <ldiego.flores98@gmail.com>
2023-05-13 17:27:40 -0700

    File Questions: problem "sum_csv_columns" completed.

--- file_questions.py
+++ file_questions.py
@@ -1,3 +1,6 @@
+import csv
+
+
 def find_phrase_in_file(phrase, file_name):
     """Function that takes a phrase and a text file as inputs. The function returns True if the phrase is found in the document and returns False otherwise."""
 
@@ -9,3 +12,20 @@ def find_phrase_in_file(phrase, file_name):
                 return True
     # doesn't find the phrase, then return False.
     return False
+
+
+def sum_csv_columns(column, file_name):
+    """Give a Comma Separated File (csv) and a column number (zero being the left most column) return the sum of all the entries in that column"""
+
+    # track the sum by columns.
+    total = 0
+    # open the file csv specified in read mode by default.
+    with open(file_name) as file:
+        # create a CSV reader object using the file.
+        reader = csv.reader(file)
+        # iterates through each row.
+        for row in reader:
+            # add the value of the row in correct column.
+            total += int(row[column])
+    # return total sum of the entries in the specified column.
+    return total
--- main.py
+++ main.py
@@ -6,8 +6,11 @@ def run():
     print(math_questions.divisible_by_5_not_7(1, 60))
     print(math_questions.number_to_base(5, 3))
     print(math_questions.return_value_following_function(5, 2))
+    print(math_questions.return_value_following_function(5, 3))
     print(file_questions.find_phrase_in_file(
         "Mammography is a critical diagnostic tool for breast cancer, with the ability to detect the disease at its earliest stages.", "example.txt"))
+    print(file_questions.sum_csv_columns(0, "example.csv"))
+    print(file_questions.sum_csv_columns(2, "example.csv"))
 
 
 if __name__ == '__main__':
