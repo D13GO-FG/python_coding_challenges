d21d6b49c6ff40efa1f20b125dc72e7ed2aedde6
Diego_Flores <ldiego.flores98@gmail.com>
2023-05-15 16:40:14 -0700

    Adding comments in game.py

--- src/games.py
+++ src/games.py
@@ -3,6 +3,8 @@ class TicTacToe:
         self.board = [[" " for _ in range(3)] for _ in range(3)]
         self.current_player = "X"
 
+    # method that check the board is printed correctly base en previuos sets.
+
     def print_board(self):
         print("   1  2  3")
         for row in range(3):
@@ -10,6 +12,8 @@ class TicTacToe:
             for col in range(3):
                 print(f"[{self.board[row][col]}]", end="")
             print()
+
+    # method that get the moves of the player in console
 
     def get_move(self):
         print("\n")
@@ -19,6 +23,8 @@ class TicTacToe:
         print('-' * 28)
         print("\n")
         return row, col
+
+    # method that make the moves in the board
 
     def make_move(self, row, col):
         row, col = row - 1, col - 1
@@ -31,11 +37,15 @@ class TicTacToe:
         self.board[row][col] = self.current_player
         return True
 
+    # method that switch the pleayer once the previous player done his move
+
     def switch_player(self):
         if self.current_player == "X":
             self.current_player = "O"
         else:
             self.current_player = "X"
+
+    # method that get the winner of the game
 
     def get_winner(self):
         # check rows ---
@@ -53,6 +63,8 @@ class TicTacToe:
         if self.board[0][2] == self.board[1][1] == self.board[2][0] != " ":
             return self.board[0][2]
 
+    # method that check if board is full without winners
+
     def board_full(self):
         for row in range(3):
             for col in range(3):
@@ -60,8 +72,12 @@ class TicTacToe:
                     return False
         return True
 
+    # method that check if the game is over when there is winner or tie
+
     def game_over(self):
         return self.get_winner() or self.board_full()
+
+    # method that run the game once.
 
     def play(self):
         while not self.game_over():
