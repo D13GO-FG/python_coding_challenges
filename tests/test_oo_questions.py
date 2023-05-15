81b62d84037080bf8b23434a3251ac15114ca8f6
Diego_Flores <ldiego.flores98@gmail.com>
2023-05-14 18:51:37 -0700

    Update test cases in test_oo_questions

--- tests/test_oo_questions.py
+++ tests/test_oo_questions.py
@@ -2,9 +2,11 @@ from src.oo_questions import perfect_squares
 
 
 def test_perfect_squares():
-    list = []
-    # iterate over the values produced by the "perfect_squares" generator with a maximum value of 30
-    for square in perfect_squares(30):
-        # print each perfect square value
-        list.append(square)
-    assert list == [1, 4, 9, 16, 25]
+    # test case 1: N is 30
+    assert list(perfect_squares(30)) == [1, 4, 9, 16, 25]
+
+    # test case 2: N is 25
+    assert list(perfect_squares(25)) == [1, 4, 9, 16]
+
+    # test case 3: N is 1
+    assert list(perfect_squares(1)) == []
